07273672b9580f59f420c548978e811dcc87274e
Muhammed Enes Unal <menes_unal@hotmail.com>
2023-11-16 21:30:08 +0300

    momentum changes

---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Constants
+{
+    public const float SWORD_WEIGHT = 1f; // kg
+    public const float SWORD_DEFAULT_ROTATION_SPEED = 100f; // ms^-1
+    public const float SWORD_DEFAULT_ROTATION_SPEED_REVERSE_DURATION = 0.5f; // s
+
+    public const float SWORD_DEFAULT_MOMENTUM_DIFFERENCE_THRESHOLD_PRECENTAGE = 0.1f; // kg m^2 s^-1
+}
--- Assets/Scripts/SwordController.cs
+++ Assets/Scripts/SwordController.cs
@@ -4,12 +4,55 @@ using UnityEngine;
 
 public class SwordController : MonoBehaviour
 {
-    public float rotationSpeed = 100f;
+    public float weight = 1f; // kg
+    public float defaultRotationSpeed = 100f; // ms^-1
+    private float currentRotationSpeed; // ms^-1
+    private bool isReversed = false;
+
+    void Start()
+    {
+        currentRotationSpeed = defaultRotationSpeed;
+    }
 
     void Update()
     {
-        // Rotate the sword around the player
-        transform.RotateAround(transform.parent.position, Vector3.forward, rotationSpeed * Time.deltaTime);
+        float direction = isReversed ? -1 : 1;
+        transform.RotateAround(transform.parent.position, Vector3.forward, direction * currentRotationSpeed * Time.deltaTime);
+    }
+
+    public void ReverseRotation()
+    {
+        isReversed = !isReversed;
+    }
+
+    public void ChangeSpeedTemporarily(float targetSpeed, float duration)
+    {
+        StartCoroutine(AdjustSpeed(targetSpeed, duration));
+    }
+
+    private IEnumerator AdjustSpeed(float targetSpeed, float duration)
+    {
+        float time = 0;
+        float initialSpeed = currentRotationSpeed;
+
+        // Increase speed to targetSpeed
+        while (time < duration / 2)
+        {
+            currentRotationSpeed = Mathf.Lerp(initialSpeed, targetSpeed, time / (duration / 2));
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        // Decrease speed back to default
+        time = 0; // reset time for the decreasing phase
+        while (time < duration / 2)
+        {
+            currentRotationSpeed = Mathf.Lerp(targetSpeed, defaultRotationSpeed, time / (duration / 2));
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        currentRotationSpeed = defaultRotationSpeed; // ensure it's set back to default
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -18,8 +61,24 @@ public class SwordController : MonoBehaviour
         // Check if the sword collided with another sword
         if (other.CompareTag("Sword"))
         {
+            SwordController otherSword = other.GetComponent<SwordController>();
             // Change the rotation direction
-            rotationSpeed *= -1f;
+
+            // Change the rotation speed according to the sword's momentum comparison
+            float otherSwordMomentum = otherSword.weight * otherSword.currentRotationSpeed;
+            float thisSwordMomentum = weight * currentRotationSpeed;
+            float momentumDifference = otherSwordMomentum - thisSwordMomentum;
+
+            if (momentumDifference > 0)
+            {
+                // The other sword has more momentum, so this sword's rotation speed should be increased
+                ChangeSpeedTemporarily(defaultRotationSpeed + momentumDifference / weight, 0.5f);
+            }
+            else if (momentumDifference < 0)
+            {
+                // This sword has more momentum, so the other sword's rotation speed should be increased
+                otherSword.ChangeSpeedTemporarily(otherSword.defaultRotationSpeed - momentumDifference / otherSword.weight, 0.5f);
+            }
         }
     }
 }
