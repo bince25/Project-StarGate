702c73b4cd9a92c2c006eb5499d636ea02483248
Baran İNCE <incebaran25@gmail.com>
2023-11-17 14:24:48 +0300

    Eny Animation and some Lights

--- Assets/Scripts/AIs/BasicEnemyAI.cs
+++ Assets/Scripts/AIs/BasicEnemyAI.cs
@@ -5,6 +5,8 @@ public class BasicEnemyAI : MonoBehaviour
 {
     private NavMeshAgent agent;
     public Transform playerTransform;
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
 
     void Awake()
     {
@@ -12,17 +14,35 @@ public class BasicEnemyAI : MonoBehaviour
         agent.updateRotation = false;
         agent.updateUpAxis = false;
         playerTransform = PlayerController.Instance.transform;
-    }
-    void Start()
-    {
 
-    }
+        // Get the Animator component
+        animator = GetComponent<Animator>();
 
+        // Get the SpriteRenderer component
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
     void Update()
     {
         if (playerTransform != null)
         {
             agent.SetDestination(playerTransform.position);
+
+            // Set IsMoving parameter based on agent's velocity
+            bool isMoving = agent.velocity.magnitude > 0.1f;
+            animator.SetBool("IsMoving", isMoving);
+            animator.SetFloat("Speed", agent.velocity.magnitude);
+
+            // Flip the sprite based on the direction of movement
+            if (isMoving)
+            {
+                UpdateSpriteFlip(agent.velocity.x);
+            }
         }
+    }
+
+    void UpdateSpriteFlip(float direction)
+    {
+        // Flip the sprite based on the direction of movement
+        spriteRenderer.flipX = direction < 0;
     }
 }
