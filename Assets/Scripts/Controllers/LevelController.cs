ea97702d027b739f81944505b3ce8bb7514a7693
Muhammed Enes Unal <menes_unal@hotmail.com>
2023-11-19 13:33:14 +0300

    Level 2 fixes

--- Assets/Scripts/Controllers/LevelController.cs
+++ Assets/Scripts/Controllers/LevelController.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -8,9 +9,13 @@ public class LevelController : MonoBehaviour
     public AudioClip levelMusic;
     public AudioSource audioSource;
 
+    public int requiredGearCount = 10;
+
     void Start()
     {
         audioSource = SoundManager.Instance.PlayMusic(levelMusic, 1.5f);
+
+        PlayLoopingBounceAnimation();
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -24,7 +29,7 @@ public class LevelController : MonoBehaviour
 
     public void EndLevel()
     {
-        if (ResourceManager.Instance.gearCount > 0)
+        if (ResourceManager.Instance.gearCount >= requiredGearCount)
         {
             LoadManager.Instance.LoadSceneWithTransition((Levels)(SceneManager.GetActiveScene().buildIndex + 1));
         }
@@ -34,5 +39,20 @@ public class LevelController : MonoBehaviour
     {
         yield return new WaitForSeconds(.7f);
         LoadManager.Instance.LoadSceneWithTransition((Levels)(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    void PlayLoopingBounceAnimation()
+    {
+        // Use DoTween to create a looping bounce animation
+        Sequence bounceSequence = DOTween.Sequence();
+
+        bounceSequence.Append(transform.DOScale(new Vector3(5.5f, 5.5f, 1.0f), 0.5f)
+            .SetEase(Ease.OutQuad));
+
+        bounceSequence.Append(transform.DOScale(new Vector3(5f, 5f, 1.0f), 0.5f)
+            .SetEase(Ease.InQuad));
+
+        // Set the animation to loop infinitely
+        bounceSequence.SetLoops(-1);
     }
 }
